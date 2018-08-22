5facf70f847e7bcf96f3cc9da1d41c1ab00ff30d
Edison Gudiño <egudino484@puce.edu.ec>
2018-08-21 22:53:50 -0500

    Primer commit conexion con firebase

---
+++ angular_puce/paginaadministrador/cargarbase.js
@@ -0,0 +1,12 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.3.1/firebase.js"></script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAXHMmzJHql7eXjRO7wHsbqtRHaqkOyTOo",
+    authDomain: "tutienda-5ef10.firebaseapp.com",
+    databaseURL: "https://tutienda-5ef10.firebaseio.com",
+    projectId: "tutienda-5ef10",
+    storageBucket: "tutienda-5ef10.appspot.com",
+    messagingSenderId: "586598065591"
+  };
+  firebase.initializeApp(config);
